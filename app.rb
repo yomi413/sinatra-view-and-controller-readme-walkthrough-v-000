ce2096d2d4bed52c0f3a2fbe093f396a4477c260
Yomaira Escano <yjescano@gmail.com>
2018-03-23 04:24:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,8 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    original_string = params["string"]
-    @reversed_string = original_string.reverse
+    puts params
+    # original_string = params["string"]
+    # @reversed_string = original_string.reverse
     erb :reversed
   end
 
